dd30c58869933d5e0fffc7a0141d79f1506b7f90
andreu55 <anduwet2@gmail.com>
2017-09-28 00:18:49 +0200

    test

--- index.php
+++ index.php
@@ -10,8 +10,9 @@ $cache = __DIR__ . '/cache';
 $blade = new Blade($views, $cache);
 
 // Aqui están los datos para pasar a la vista
-require_once 'views/datos.php';
-require_once 'views/functions.php';
+require_once __DIR__.'/views/datos.php';
+require_once __DIR__.'/views/functions.php';
 
+echo asset('vendor/bootstrap/css/bootstrap.min.css');
 
-echo $blade->view()->make('welcome', $data)->render();
+// echo $blade->view()->make('welcome', $data)->render();
