651319bb11a4fa8471bd5e4c9c0f76c10e23ea7d
andreu55 <anduwet2@gmail.com>
2018-02-14 17:44:16 +0100

    Migración a https

--- views/functions.php
+++ views/functions.php
@@ -2,10 +2,10 @@
 
 // Simulamos la funcion url()
 function url($url) {
-  return "http://" . $_SERVER['SERVER_NAME'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . $url;
+  return "https://" . $_SERVER['SERVER_NAME'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . $url;
 }
 
 // Simulamos la funcion asset()
 function asset($url) {
-  return "http://" . $_SERVER['SERVER_NAME'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . "/public/" . $url;
+  return "https://" . $_SERVER['SERVER_NAME'] . substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . "/public/" . $url;
 }
