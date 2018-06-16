42839d093e9443816672d8801c221d1d726214af
Andreu García <anduwet2@gmail.com>
2018-06-16 17:01:37 +0200

    Cambios en info

--- views/layouts/info.blade.php
+++ views/layouts/info.blade.php
@@ -86,13 +86,15 @@
               </div>
               <h4 class="cover-title">
                 {{-- Buenas! --}}
-                <i class="fa fa-fw fa-quote-left" aria-hidden="true" style="font-size:1.3em"></i> <?= $texto ?>
+                <?= $texto ?>
+                <i class="fa fa-fw fa-smile-o" aria-hidden="true" style="font-size:1.3em"></i>
               </h4>
               <div class="flow-text midescripcion">
-                <p>Soy programador de aplicaciones web por las mañanas y profesor de Lindy Hop por las tardes, si no me encuentras frente a la pantalla, búscame en la pista de baile <i class="fa fa-smile-o"></i>.</p>
-                <p>Me apasiona solucionar problemas y encontrar la manera más eficiente de hacer las cosas, necesito definir un proyecto lo mejor posible con el cliente para optimizar cada aplicación desde el principio y evitar malentendidos, ¡aunque los cambios son inevitables!</p>
+                <p>Programador de aplicaciones web por las mañanas, profesor de Lindy Hop por las tardes, si no me encuentras frente a la pantalla, ¡búscame en la pista de baile!</p>
+                <p>Me apasiona solucionar problemas y encontrar la manera más eficiente de hacer las cosas, me gusta definir un proyecto lo mejor posible para que cada aplicación sea genial desde el principio y así evitar malentendidos, ¡aunque los cambios son inevitables!</p>
                 <p>Por contrapartida, para gran frustración del project manager, me cuesta no intentar solucionar cada error del código, es como si llevaras el coche al mecánico porque no funcionan las luces y además te arreglara la dirección y la correa de distribución porque estaban a punto de estropearse, aunque he trabajado en agencias y consultorías informáticas y he aprendido también a limitarme al error que nos pide el cliente cuando hay que hacerlo y el tiempo es lo más importante.</p>
                 <p>Me encanta trabajar en BackEnd, crear APIs que nutran las aplicaciones, pero siempre me las he tenido que arreglar para saber hacerlo todo, así que no me sorprende encontrarme en Front, diseñando como buenamente puedo o pensando en la usabilidad del usuario para que todo vaya como la seda. Me encantaría estar en un equipo experimentado en el que podamos crecer aportando y aprendiendo juntos.</p>
+                <p><i class="fa fa-fw fa-quote-left" aria-hidden="true" style="font-size:1.3em"></i> - Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.</p>
               </div>
               <div class="signature">
                 <img src="{{ asset('img/firma_andreu.png') }}" alt="" height="100" class="img-responsive">
